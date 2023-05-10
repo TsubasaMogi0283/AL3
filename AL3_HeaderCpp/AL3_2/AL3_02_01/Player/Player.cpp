64c1e24d91276f473ea8b7fadc3369bc46a4b1d7
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-10 14:47:09 +0900

    「初期化処理」

--- AL3_HeaderCpp/AL3_2/AL3_02_01/Player/Player.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_01/Player/Player.cpp
@@ -1,7 +1,15 @@
 #include <AL3_HeaderCpp/AL3_2/AL3_02_01/Player/Player.h>
 
-void Player::Initialize() {
+#include <cassert>
 
+void Player::Initialize(Model* model,uint32_t textureHandle) {
+	//NULLポインタチェック
+	assert(model);
+
+	//引数として受け取ったデータをメンバ変数に記録する
+
+	//ワールド変数の初期化
+	worldTransform_.Initialize();
 }
 
 void Player::UpDate() {
