1acdad648dcd27059201fb6e81c3df2aa45d4727
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-12 20:40:24 +0900

    PLayerのクラスの下準備

---
+++ AL3_HeaderCpp/AL3_2/AL3_02_01/Player/Player.cpp
@@ -0,0 +1,31 @@
+﻿#include <AL3_HeaderCpp/AL3_2/AL3_02_01/Player/Player.h>
+
+#include <cassert>
+
+//初期化
+void Player::Initialize(Model* model,uint32_t textureHandle) { 
+	
+
+	assert(model);
+
+	//引数として受け取ったデータをメンバ変数に記録する
+	this->model_ = model;
+	this->textureHandle_ = textureHandle;
+
+	//ワールド変数の初期化
+	worldTransform_.Initialize();
+}
+
+//更新
+void Player::UpDate() { 
+	//行列を定数バッファに転送
+	worldTransform_.TransferMatrix(); 
+}
+
+//描画
+void Player::Draw(ViewProjection viewProjection) { 
+	model_->Draw(
+		this->worldTransform_, 
+		viewProjection, 
+		this->textureHandle_);
+}
