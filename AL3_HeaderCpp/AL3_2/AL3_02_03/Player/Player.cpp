a67e2977f144929d38da37f7f7aff279aced5ada
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-13 01:44:14 +0900

    初期化処理

--- AL3_HeaderCpp/AL3_2/AL3_02_03/Player/Player.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_03/Player/Player.cpp
@@ -1,1 +1,35 @@
-﻿
+﻿#include <AL3_HeaderCpp/AL3_2/AL3_02_03/Player/Player.h>
+
+void Player::Initialize() { 
+	input_ = Input::GetInstance(); 
+}
+
+
+
+
+
+
+void Player::Rotate() { 
+	//回転の速さ(ラジアン/frame)
+	const float ROT_SPEED = 0.02f;
+
+	//押した方向で移動ベクトルを変更
+	if (input_->PushKey(DIK_A)) {
+		worldTransform_.rotation_.y = -ROT_SPEED;
+	} 
+	else if (input_->PushKey(DIK_A)) {
+		worldTransform_.rotation_.y = ROT_SPEED;
+	}
+
+
+
+}
+
+void Player::Update() { 
+	//Updateから呼び出す
+	Rotate();
+
+
+
+
+}
--- AL3_HeaderCpp/AL3_2/AL3_02_03/Player/Player.h
+++ AL3_HeaderCpp/AL3_2/AL3_02_03/Player/Player.h
@@ -1,16 +1,23 @@
 ﻿#pragma once
+#include <Input.h>
+#include <WorldTransform.h>]
 
 class Player {
 public:
 
+	void Initialize();
 
+	void Update();
 
+	void Rotate();
 
+	void Draw();
 
 
 private:
 
+	Input* input_ = nullptr;
 
-
+	WorldTransform worldTransform_;
 
 };
