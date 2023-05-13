ed1078c566b36ed8c66fe1ab3ddeff96f70e4755
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-13 16:48:45 +0900

    コミットし直し

--- 3d/WorldTransform.h
+++ 3d/WorldTransform.h
@@ -52,13 +52,35 @@ struct WorldTransform {
 
 class  WorldTransform {
 public:
-
+	/// <summary>
+	/// 初期化
+	/// </summary>
+	void Initialize();
+	/// <summary>
+	/// 定数バッファ生成
+	/// </summary>
+	void CreateConstBuffer();
+	/// <summary>
+	/// マッピングする
+	/// </summary>
+	void Map();
+	/// <summary>
+	/// 行列を転送する
+	/// </summary>
+	void TransferMatrix();
 	/// <summary>
 	/// 行列を計算・転送する
 	/// </summary>
 	void UpdateMatrix(); 
 
-private:
+public:
+
+	
+
+	// 定数バッファ
+	Microsoft::WRL::ComPtr<ID3D12Resource> constBuff_;
+	// マッピング済みアドレス
+	ConstBufferDataWorldTransform* constMap = nullptr;
 
 	// ローカルスケール
 	Vector3 scale_ = {1, 1, 1};
@@ -66,6 +88,12 @@ private:
 	Vector3 rotate_ = {0, 0, 0};
 	// ローカル座標
 	Vector3 translation_ = {0, 0, 0};
+
+
+	// ローカル → ワールド変換行列
+	Matrix4x4 matWorld_;
+	// 親となるワールド変換へのポインタ
+	const WorldTransform* parent_ = nullptr;
 
 };
 
