319c7c794c7ee8dcb7a20450cc23db2ce2d6ef5b
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-17 00:10:06 +0900

    Enemyクラス作成

---
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Player/Player.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include <list>
+
+#include <input/Input.h>
+#include <Model.h>
+#include <WorldTransform.h>
+#include <AL3_HeaderCpp/AL3_2/AL3_02_03/Function/Function.h>
+#include <AL3_HeaderCpp/AL3_2/AL3_02_03/PlayerBullet/PlayerBullet.h>
+
+class Player {
+public:
+	//デストラクタ
+	~Player();
+
+	//初期化
+	void Initialize(Model* model,uint32_t textureHandle);
+
+	//回転
+	void Rotate();
+	//攻撃
+	void Attack();
+
+	//更新
+	void UpDate();
+
+	//描画
+	void Draw(ViewProjection viewProjection);
+private:
+		
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+	//自キャラ
+	Player* player_ = nullptr;
+
+	Input* input_ = nullptr;
+
+	//弾
+	//メンバ「変数」なのでprivate
+	PlayerBullet* bullet_ = nullptr;
+
+	//STL(Standard Template Library)...「データの出し入れ、整理する」ための便利な仕組みを提供するlib
+	// 
+	//list(双方向リスト)の導入
+	//とりあえず弾を沢山出したい時に使うのがおすすめらしい。ただどの順番で消えるか分からない
+	//複数形だからsつけるの忘れないでね
+	std::list<PlayerBullet*> bullets_;
+
+
+
+};
+
+
---
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/PlayerBullet/PlayerBullet.cpp
@@ -0,0 +1,42 @@
+﻿#include <cassert>
+#include <AL3_HeaderCpp/AL3_2/AL3_02_03/PlayerBullet/PlayerBullet.h>
+#include <AL3_HeaderCpp/AL3_2/AL3_02_03/Function/Function.h>
+
+void PlayerBullet::Initialize(Model* model, const Vector3& position,const Vector3& velocity) { 
+	
+	//NULLチェック
+	assert(model);
+
+	model_ = model;
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("AL3_Resources/AL3_2/AL3_2_3/bullet.png");
+
+	//ワールドトランスフォームの初期化
+	//中にあるよ
+	worldTransform_.Initialize();
+	//引数で受け取った初期座標をセット
+	worldTransform_.translation_ = position;
+	velocity_ = velocity;
+
+}
+
+void PlayerBullet::Update() { 
+
+	//座標を移動させる(1フレーム分足す)
+	//ベクトルの足し算
+	worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
+
+	//時間経過でデス
+	if (--deathTimer_ <= 0) {
+		isDead_ = true;
+	}
+
+
+	//ワールドトランスフォームの更新
+	worldTransform_.UpdateMatrix(); 
+}
+
+void PlayerBullet::Draw(const ViewProjection& viewProjection) { 
+	//自キャラと同じ処理なので出来れば継承を使うといいよ！
+	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+}
