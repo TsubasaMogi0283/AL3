28c61fa5dbb7d4da4ef5d24b3af87582a341aca8
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-17 00:20:41 +0900

    メンバ変数の追加

---
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
@@ -0,0 +1,13 @@
+#include <AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h>
+
+void Enemy::Initialize() {
+
+}
+
+void Enemy::Update() {
+
+}
+
+void Enemy::Draw() {
+
+}
--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+
+class Enemy {
+public:
+
+	void Initialize();
+
+	void Update();
+
+	void Draw();
+
+
+private:
+	
+	//ワールドトランスフォーム
+	WorldTransform* worldTransform_;
+	//モデル
+	Model* model_=nullptr;
+	//テクスチャ
+	uint32_t textureHandle_ = 0u;
+
+
+
+};
