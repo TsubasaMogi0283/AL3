5a18768682e6b31f86dfb781c05e92ce758a4410
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-17 13:40:48 +0900

    Enemy追加

--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
@@ -1,13 +1,40 @@
+#include <cassert>
+
 #include <AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h>
 
-void Enemy::Initialize() {
+void Enemy::Initialize(Model* model,const Vector3& position,const Vector3& velocity) {
+
+	//assertも
+	assert(model);
+
+	//モデルも忘れずに
+	model_ = model;
+
+
+	//テクスチャ読み込み
+	textureHandle_ = TextureManager::Load("AL3_Resources\AL3_2\AL3_2_6\Enemy\Enemy.png");
+
+	//ワールドトランスフォームの初期化
+	worldTransform_->Initialize();
+
+	//初期座標・速度の設定を行う
+	worldTransform_->translation_ = position;
+	velocity_ = velocity;
 
 }
 
 void Enemy::Update() {
 
+	//移動
+	worldTransform_->translation_ = Add(worldTransform_->translation_, velocity_);
+
+	//行列の更新
+	worldTransform_->UpdateMatrix();
+
 }
 
-void Enemy::Draw() {
+void Enemy::Draw(const ViewProjection& viewProjection) { 
+	
+	model_->Draw(viewProjection);
 
 }
--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
@@ -1,15 +1,18 @@
 #pragma once
 #include "Model.h"
 #include "WorldTransform.h"
+#include "ViewProjection.h"
+
+#include <AL3_HeaderCpp/AL3_2/AL3_02_06/Function/Function.h>
 
 class Enemy {
 public:
 
-	void Initialize();
+	void Initialize(Model* model,const Vector3& position,const Vector3& velocity_);
 
 	void Update();
 
-	void Draw();
+	void Draw(const ViewProjection& viewProjection);
 
 
 private:
@@ -17,10 +20,11 @@ private:
 	//ワールドトランスフォーム
 	WorldTransform* worldTransform_;
 	//モデル
-	Model* model_=nullptr;
+	Model* model_ = nullptr;
 	//テクスチャ
 	uint32_t textureHandle_ = 0u;
 
-
+	//速度
+	Vector3 velocity_;
 
 };
