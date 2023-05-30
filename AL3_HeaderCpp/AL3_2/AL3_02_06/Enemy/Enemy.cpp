4317be34d1125af46cf33eab2996dcc4a6326a3b
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-05-31 01:32:52 +0900

    1

--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
@@ -1,51 +1,3 @@
-
-//#include <cassert>
-//
-//#include <AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h>
-//#include <AL3_HeaderCpp/AL3_2/AL3_02_03/PlayerBullet/PlayerBullet.h>
-//
-//void Enemy::Initialize(Model* model,const Vector3& position,const Vector3& velocity) {
-//
-//	//assertも
-//	assert(model);
-//
-//	//モデルも忘れずに
-//	this->model_ = model;
-//
-//
-//	//テクスチャ読み込み
-//	textureHandle_ = TextureManager::Load("AL3_Resources\AL3_2\AL3_2_6\Enemy\Enemy.png");
-//
-//	//ワールドトランスフォームの初期化
-//	worldTransform_->Initialize();
-//
-//	//初期座標・速度の設定を行う
-//	worldTransform_->translation_ = position;
-//	velocity_ = velocity;
-//
-//}
-//
-//void Enemy::Update() {
-//
-//	//移動
-//	worldTransform_->translation_ = Add(worldTransform_->translation_, velocity_);
-//
-//	//行列の更新
-//	worldTransform_->UpdateMatrix();
-//
-//}
-//
-//void Enemy::Draw(const ViewProjection& viewProjection) { 
-//	model_->Draw(worldTransform_, viewProjection, textureHandle_);
-//	//model_->Draw(this->worldTransform_, viewProjection, this->textureHandle_);
-//}
-//
-//void PlayerBullet::Draw(const ViewProjection& viewProjection) { 
-//	//自キャラと同じ処理なので出来れば継承を使うといいよ！
-//	model_->Draw(worldTransform_, viewProjection, textureHandle_);
-//}
-
-
 #include <cassert>
 #include <AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h>
 #include <AL3_HeaderCpp/AL3_2/AL3_02_03/Function/Function.h>
@@ -65,36 +17,44 @@ void Enemy::Initialize(Model* model, const Vector3& position,const Vector3& velo
 	worldTransform_.Initialize();
 	//引数で受け取った初期座標をセット
 	worldTransform_.translation_ = position;
-	enemyVelocity_ = velocity;
+	this->enemyVelocity_ = velocity;
 
 }
 
-void Enemy::ApproachUpdate() {
-	//移動(ベクトルの加算)
-		
-	worldTransform_.translation_ = Add(worldTransform_.translation_, enemyVelocity_);
-	//規定の位置に到達したら離脱
-	if (worldTransform_.translation_.z < 0.0f) {
-		phase_ = Phase::Leave;
-	}
-	
+//void Enemy::ApproachUpdate() {
+//	//移動(ベクトルの加算)
+//		
+//	worldTransform_.translation_ = Add(worldTransform_.translation_, enemyVelocity_);
+//	//規定の位置に到達したら離脱
+//	if (worldTransform_.translation_.z < 0.0f) {
+//		phase_ = Phase::Leave;
+//	}
+//	
+//}
+//
+//void Enemy::LeaveUpdate() {
+//	//移動(ベクトルを加算)
+//	worldTransform_.translation_.x += 0.2f;
+//	worldTransform_.translation_.y += 0.02f;
+//}
+
+
+
+void Enemy::SetEnemyTranslate(Vector3 translation) {
+	worldTransform_.translation_.x = translation.x;
+	worldTransform_.translation_.y = translation.y;
+	worldTransform_.translation_.z = translation.z;
+
 }
 
-void Enemy::LeaveUpdate() {
-	//移動(ベクトルを加算)
-	worldTransform_.translation_.x += 0.2f;
-	worldTransform_.translation_.y += 0.02f;
-}
-
-
-void (Enemy::*Enemy::spFuncTable[])()={
-
-	//0
-	&Enemy::ApproachUpdate,
-	//1
-	&Enemy::LeaveUpdate,
-
-};
+//void (Enemy::*Enemy::spFuncTable[])()={
+//
+//	//0
+//	&Enemy::ApproachUpdate,
+//	//1
+//	&Enemy::LeaveUpdate,
+//
+//};
 
 void Enemy::Update() { 
 
@@ -114,8 +74,10 @@ void Enemy::Update() {
 	
 	//メンバ関数ポインタに入っている関数を呼び出す
 	//ここではAppriachUpdate
-	(this->*spFuncTable[static_cast<size_t>(phase_)])();
-
+	//(this->*spFuncTable[static_cast<size_t>(phase_)])();
+	
+	//Updateが原因
+	state_->Update();
 
 
 	//座標を移動させる(1フレーム分足す)
@@ -131,3 +93,65 @@ void Enemy::Draw(const ViewProjection& viewProjection) {
 	//自キャラと同じ処理なので出来れば継承を使うといいよ！
 	model_->Draw(worldTransform_, viewProjection, textureHandle_);
 }
+
+
+
+
+
+
+
+Enemy::Enemy() {
+	//最初はApproach
+	state_ = new EnemyStateApproach();
+}
+
+Enemy::~Enemy() {
+	delete state_;
+}
+
+void Enemy::ChangeState(BaseEnemyState* newState) { 
+	delete state_; 
+	this->state_ = newState;
+}
+
+
+BaseEnemyState::BaseEnemyState(){
+
+}
+
+//接近
+void EnemyStateApproach::Update() {
+	
+
+	//ここが原因
+	//Getterなどで設定しよう！
+
+	//enemyNewTranslate_ = enemy_->GetEnemyTranslate();
+	
+
+	enemy_->worldTransform_.translation_ = Add(enemy_->worldTransform_.translation_, {0.0f,0.0f,-0.2f});
+	//enemy_->SetEnemyTranslate(enemyNewTranslate_);
+
+
+	//{0.0f, 0.0f, kEnemySpeed_};
+
+	//規定の位置に到達したら離脱
+	if (enemy_->worldTransform_.translation_.z < 0.0f) {
+		enemy_->ChangeState(new EnemyStateLeave());
+		//phase_ = Phase::Leave;
+	}
+}
+
+void EnemyStateLeave::Update() {
+	//移動(ベクトルを加算)
+	enemy_->SetEnemyTranslate(
+	    {enemy_->worldTransform_.translation_.x + 0.2f, 
+		enemy_->worldTransform_.translation_.y + 0.02f,
+	     enemy_->worldTransform_.translation_.z});
+
+	//worldTransform_.translation_.x += 0.2f,
+	//worldTransform_.translation_.y += 0.02f)
+	
+}
+
+
--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.h
@@ -4,78 +4,29 @@
 #include <WorldTransform.h>
 #include <Model.h>
 
-class Enemy {
-public:
+
+class Enemy;
+class BaseEnemyState;
+class EnemyStateApproach;
+class EnemyStateLeave;
 
 
-	//Initialize(mode,position,velocity)
-	void Initialize(Model* model,const Vector3& position,const Vector3& velocity);
-
-	void Update();
-
-	//ビュープロジェクション
-	void Draw(const ViewProjection& viewProjection);
-
-	Vector3 GetEnemyPosition() { 
-		return enemyPosition_;
-	}
-	Vector3 GetEnemyVelocity() { 
-		return enemyVelocity_;
-	}
-
-	void ApproachUpdate();
-
-	void LeaveUpdate();
 
 
-	//StatePattern
-	void ChangeState(BaseEnemyState*);
-
-private:
-
-	//ワールド変換データ
-	WorldTransform worldTransform_;
-	//モデルのポインタ
-	Model* model_ = nullptr;
-	//テクスチャハンドル
-	uint32_t textureHandle_ = 0u;
-
-	//速度
-	const float kEnemySpeed_ = -0.2f;
-	Vector3 enemyPosition_ = {0.0f, 3.0f, 20.0f};
-	Vector3 enemyVelocity_ = {0.0f, 0.0f, kEnemySpeed_};
-	
-	
-	
-	//enum宣言
-	enum class Phase {
-		Approach,	//接近
-		Leave,		//離脱
-	};
-
-	Phase phase_ = Phase();
-
-	
-
-	//メンバ関数ポインタのテーブル
-	static void (Enemy::*spFuncTable[])();
-
-
-	//statepattern
-	BaseEnemyState* state_;
-
-};
 
 
 
 class BaseEnemyState {
 public:
-	virtual void Update();
+	BaseEnemyState();
+
+	virtual void Update()=0;
 
 
 protected:
-	Enemy* enemy_;
+	Enemy* enemy_=nullptr;
 
+	Vector3 enemyNewTranslate_ = {};
 
 };
 
@@ -87,4 +38,82 @@ public:
 class EnemyStateLeave :public BaseEnemyState{
 public:
 	void Update() override;
+};
+
+
+
+
+
+class Enemy {
+public:
+	Enemy();
+	~Enemy();
+
+	//Initialize(mode,position,velocity)
+	void Initialize(Model* model,const Vector3& position,const Vector3& velocity);
+
+	void Update();
+
+	//ビュープロジェクション
+	void Draw(const ViewProjection& viewProjection);
+
+	Vector3 GetEnemyTranslate() { 
+		return enemyTranslate_;
+	}
+	Vector3 GetEnemyVelocity() { 
+		return enemyVelocity_;
+	}
+	
+	
+
+	void SetEnemyTranslate(Vector3 translation);
+	
+	
+
+	//void ApproachUpdate();
+
+	//void LeaveUpdate();
+
+
+	//StatePattern
+	void ChangeState(BaseEnemyState* newState);
+
+public:
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデルのポインタ
+	Model* model_ = nullptr;
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+
+
+	//速度
+	const float kEnemySpeed_ = -0.2f;
+	Vector3 enemyTranslate_ = {0.0f, 3.0f, 20.0f};
+	
+
+	Vector3 enemyVelocity_ = {0.0f, 0.0f, kEnemySpeed_};
+	
+
+
+	
+	
+	////enum宣言
+	//enum class Phase {
+	//	Approach,	//接近
+	//	Leave,		//離脱
+	//};
+
+	//Phase phase_ = Phase();
+
+	
+
+	//メンバ関数ポインタのテーブル
+	//static void (Enemy::*spFuncTable[])();
+
+
+	//statepattern
+	BaseEnemyState* state_=nullptr;
+
 };
