08f0ddc6cd802b6a1b17558e4cbd6ac33f176b24
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-06-14 15:55:41 +0900

    1

--- AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_06/Enemy/Enemy.cpp
@@ -13,21 +13,17 @@ BaseEnemyState::BaseEnemyState() {
 }
 
 
-void Enemy::Initialize(Model* model, const Vector3& position,const Vector3& velocity) { 
+void Enemy::Initialize(Model* model,uint32_t textureHandle) { 
 	
 	//NULLチェック
 	assert(model);
 
 	model_ = model;
-	//テクスチャ読み込み
-	textureHandle_ = TextureManager::Load("AL3_Resources/AL3_2/AL3_2_6/Enemy/Enemy.png");
-
+	
+	textureHandle_ = textureHandle;
 	//ワールドトランスフォームの初期化
 	//中にあるよ
 	worldTransform_.Initialize();
-	//引数で受け取った初期座標をセット
-	this->worldTransform_.translation_ = position;
-	this->enemyVelocity_ = velocity;
 
 }
 
