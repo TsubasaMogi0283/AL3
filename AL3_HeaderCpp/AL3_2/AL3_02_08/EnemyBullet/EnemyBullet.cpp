62b32eee4139888caf7c3546d3f00e921d3f95c7
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-06-28 00:05:18 +0900

    何か変だった

--- AL3_HeaderCpp/AL3_2/AL3_02_08/EnemyBullet/EnemyBullet.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_08/EnemyBullet/EnemyBullet.cpp
@@ -18,6 +18,45 @@ void EnemyBullet::Initialize(Model* model, const Vector3& position,const Vector3
 	worldTransform_.translation_ = position;
 	velocity_ = velocity;
 
+	//Z方向に伸びた形状
+	worldTransform_.scale_.x = 0.5f;
+	worldTransform_.scale_.y = 0.5f;
+	worldTransform_.scale_.z = 3.0f;
+
+	
+	 
+	
+	//ロール・ピッチ・ヨー
+	// MTでやったやつだけど一応もう一回復習として
+	//ロール...Z軸に回転
+	//ピッチ...X軸に回転(仰角)
+	//ヨー...Y軸に回転(方位角)
+
+	#pragma region 解法1
+	//atan2...ArcTangent
+	//底辺と高さから角度を求める
+	//atan2(高さ,底辺)
+	//Y軸回りの角度
+	worldTransform_.rotation_.y = std::atan2(velocity_.z,velocity_.x);
+	//Y軸周りに-θy回す回転行列を計算
+	Matrix4x4 rotateYMatrix = MakeRotateYMatrix(worldTransform_.rotation_.y);
+	//velocity_に回転行列を掛け算してvelocityZを求める
+	Vector3 velocityZ = TransformNormal(velocity_, rotateYMatrix);
+	
+	//X軸周り角度
+	worldTransform_.rotation_.x = std::atan2(velocity_.y, velocity_.z);
+	
+	#pragma endregion
+
+
+	#pragma region 解法2
+	//Y軸周り角度(θy)
+	//worldTransform_.rotation_.y = std::atan2();
+
+
+
+	#pragma endregion
+
 }
 
 
