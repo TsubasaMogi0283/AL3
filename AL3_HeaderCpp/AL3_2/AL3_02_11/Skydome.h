c3fb486a1d64c8a258d67efe517036e7c97ec667
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-07-14 14:52:34 +0900

    メンバ変数の追加

--- AL3_HeaderCpp/AL3_2/AL3_02_11/Skydome.h
+++ AL3_HeaderCpp/AL3_2/AL3_02_11/Skydome.h
@@ -1,1 +1,25 @@
 #pragma once
+#include <WorldTransform.h>
+#include <Model.h>
+
+class Skydome {
+public:
+	//初期化
+	void Initialize();
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw();
+
+
+private:
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+
+};
+
