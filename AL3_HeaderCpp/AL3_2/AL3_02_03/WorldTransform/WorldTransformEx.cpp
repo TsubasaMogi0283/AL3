e2707345a3e482d33c4f7a11680e506f689c2b75
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-07-24 15:12:15 +0900

    親子関係の反映

--- AL3_HeaderCpp/AL3_2/AL3_02_03/WorldTransform/WorldTransformEx.cpp
+++ AL3_HeaderCpp/AL3_2/AL3_02_03/WorldTransform/WorldTransformEx.cpp
@@ -8,6 +8,11 @@ void WorldTransform::UpdateMatrix() {
 	//SRT合成
 	matWorld_ = MakeAffineMatrix(scale_, rotation_, translation_);
 
+	//親があれば親のワールド行列を掛ける
+	if (parent_) {
+		matWorld_ = Multiply(matWorld_, parent_->matWorld_);
+	}
+
 	//定数バッファに転送
 	TransferMatrix();
 
