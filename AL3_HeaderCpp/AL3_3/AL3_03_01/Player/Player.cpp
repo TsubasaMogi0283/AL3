5fab5fa92d6ceeb8c0e9415a77c68d4d9c6fda18
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-07-27 02:46:52 +0900

    1

--- AL3_HeaderCpp/AL3_3/AL3_03_01/Player/Player.cpp
+++ AL3_HeaderCpp/AL3_3/AL3_03_01/Player/Player.cpp
@@ -1,23 +1,31 @@
 ﻿#include "AL3_HeaderCpp/AL3_3/AL3_03_01/Player/Player.h"
 #include <cassert>
 
-void Player::Initialize(Model* model, uint32_t textureHandle) { 
-	assert(model_);
+//初期化
+void Player::Initialize(Model* model,uint32_t textureHandle) { 
+	
 
+	assert(model);
+
+	//引数として受け取ったデータをメンバ変数に記録する
 	this->model_ = model;
+	this->textureHandle_ = textureHandle;
 
-	textureHandle_ = textureHandle;
-
+	//ワールド変数の初期化
 	worldTransform_.Initialize();
-
 }
 
+//更新
 void Player::Update() { 
+	//行列を定数バッファに転送
 	worldTransform_.TransferMatrix(); 
 }
 
-void Player::Draw(ViewProjection& viewProjection) {
-	model_->Draw(worldTransform_, viewProjection, textureHandle_);
+//描画
+void Player::Draw(ViewProjection& viewProjection) { 
+	model_->Draw(
+		this->worldTransform_, 
+		viewProjection, 
+		this->textureHandle_);
 }
-
 
