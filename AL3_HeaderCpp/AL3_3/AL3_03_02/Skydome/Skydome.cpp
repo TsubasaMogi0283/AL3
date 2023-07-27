a2752df770471d25b778728db8346084c65830a5
翼 茂木 <k022g0130@m.neec.ac.jp>
2023-07-27 14:11:46 +0900

    計算行列を実装

---
+++ AL3_HeaderCpp/AL3_3/AL3_03_02/Skydome/Skydome.cpp
@@ -0,0 +1,36 @@
+﻿#include "Skydome.h"
+#include <AL3_HeaderCpp/AL3_3/AL3_03_02/MatrixCalculation/MatrixCalculation.h>
+
+//初期化
+void Skydome::Initialize(Model* model,uint32_t textureHandle) { 
+	this->model_ = model; 
+	this->textureHandle_ = textureHandle;
+	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+	//小さすぎたかも・・
+	worldTransform_.scale_ = {100.0f, 100.0f, 100.0f};
+	//ワールド変数の初期化
+	worldTransform_.Initialize();
+}
+
+//更新
+void Skydome::Update() {
+
+	//行列を定数バッファに転送
+	worldTransform_.TransferMatrix(); 
+
+	
+
+	worldTransform_.matWorld_ = MakeAffineMatrix(
+	    worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
+
+	//更新処理
+	//worldTransform_.UpdateMatrix();
+
+}
+
+//描画
+void Skydome::Draw(ViewProjection viewProjection) {
+	model_->Draw(
+		worldTransform_, 
+		viewProjection);
+}
---
+++ AL3_HeaderCpp/AL3_3/AL3_03_02/Skydome/Skydome.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+#include <WorldTransform.h>
+#include <Model.h>
+
+class Skydome {
+public:
+	//初期化
+	void Initialize(Model* model,uint32_t textureHandle);
+
+	//更新
+	void Update();
+
+	//描画
+	void Draw(ViewProjection viewProjection);
+
+
+private:
+
+	//ワールド変換データ
+	WorldTransform worldTransform_;
+	//モデル
+	Model* model_ = nullptr;
+
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0u;
+};
+
